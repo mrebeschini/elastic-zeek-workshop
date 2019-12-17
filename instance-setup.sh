e5dba9b0e70efcb9b1a74f51c2bb36315370fed8
Matteo Rebeschini <matteo@elastic.co>
2019-12-17 09:49:23 -0700

    Create instance-setup.sh

---
+++ instance-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                                                                                                                                                              
+CONFIG_REPOSITORY_URL="https://raw.githubusercontent.com/mrebeschini/elastic-zeek-workshop/master"
+
+wget $CONFIG_REPOSITORY_URL/beats_install.sh
