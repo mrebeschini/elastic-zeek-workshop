fdd7fee9e793d54f32bb6047731a21d2de50374c
Matteo Rebeschini <matteo@elastic.co>
2019-12-17 14:13:13 -0700

    Update instance-setup.sh

--- instance-setup.sh
+++ instance-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash                                                                                                                                                                                                                              
 CONFIG_REPOSITORY_URL="https://raw.githubusercontent.com/mrebeschini/elastic-zeek-workshop/master"
+ZEEK_DIR=/home/ubuntu/zeek
+
+mkdir $ZEEK_DIR
+chown ubuntu:ubuntu $ZEEK_DIR
 
 wget $CONFIG_REPOSITORY_URL/beats_install.sh
